2ea6b69703e5e882a4a3837a2c1894a1945652b6
deepanshu-mandy <23BCS12327@cuchd.in>
2026-04-17 10:05:09 +0530

    Create Topological_sort.cpp

---
+++ Session_Problems/12_03_26/Topological_sort.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> topoSort(int V, vector<int> adj[]) {
+
+    vector<int> indegree(V,0);
+
+    for(int i=0;i<V;i++){
+        for(int v: adj[i]){
+            indegree[v]++;
+        }
+    }
+
+    queue<int> q;
+
+    for(int i=0;i<V;i++){
+        if(indegree[i]==0)
+            q.push(i);
+    }
+
+    vector<int> topo;
+
+    while(!q.empty()){
+
+        int node=q.front();
+        q.pop();
+
+        topo.push_back(node);
+
+        for(int v: adj[node]){
+            indegree[v]--;
+
+            if(indegree[v]==0)
+                q.push(v);
+        }
+    }
+
+    return topo;
+}
